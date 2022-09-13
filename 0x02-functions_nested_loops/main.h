b31dee820d0181f2403671101fd28ed4504a8154
samm-cyber <samrodoyo@gmail.com>
2022-09-13 15:51:33 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char);
 void print_alphabet(void);
@@ -13,6 +13,6 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
-void num (int r,int n);
+void num(int r,int n);
 
 #endif
