c0461157449e1587694dd8a5f737d9bab2d6fe0a
samm-cyber <samrodoyo@gmail.com>
2022-09-13 18:23:52 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_alphabet- prints the alphabet, in lower case, followed by a new line
+*/
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ++c)
+		_putchar(ch);
+	_putchar('\n');
+}
