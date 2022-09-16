5419fe2d4dbe9f16c26759bac870f946c8657a22
samm-cyber <samrodoyo@gmail.com>
2022-09-15 18:40:17 -0700

    5-more_numbers.c updated code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,15 @@
  */
 void more_numbers(void)
 {
-	int num, count;
+	int i, j;
 
-	for (count = 0; count < = 9; count++)
+	for (i = 0; i < 10; i++)
 	{
-		for (num = 0; num < = 14; num++)
+		for (j = 0; j < 15; j++)
 		{
-			if (num > 9)
-				_putchar((num / 10) + '0');
-			_putchar((num % 10) + '0');
+			if (j > 10)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
