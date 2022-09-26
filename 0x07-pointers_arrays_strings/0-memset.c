6b7b9dd87429abb2abc4b7c9578fb9d8c5e381e7
samm-cyber <samrodoyo@gmail.com>
2022-09-26 14:59:00 -0700

    main.h

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,21 @@
 #include "main.h"
 
 /**
- * _memset - concanates two strings
+ * _memset - concantenates two strings
  * @s: pointer
  * @b: constant byte
- * @n: n bytes of memory
+ * @n: n bytes of a memory
+ *
  * Return: s
  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unasigned int i;
+
+	for (i = 0; i < n; i++, n--)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
