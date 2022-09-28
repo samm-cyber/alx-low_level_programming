4d2cb2ee65bf2cb48ecb03b02af2573b6a0727be
samm-cyber <samrodoyo@gmail.com>
2022-09-28 03:54:48 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _puts_recursion - prints a string, followed by a new line
- * @s: string toi print
+ * @s: string to print
  * Return: void
  */
 
@@ -12,6 +12,7 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
 
 	else
