90181b915a0a7dda240842b16bfc6d4efecf71fe
samm-cyber <samrodoyo@gmail.com>
2022-09-30 16:35:07 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int result, num1, prod;
+	int num1, num2, prod;
 
 	if (argc != 3)
 	{
@@ -18,9 +18,9 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
 
 	printf("%d\n", prod);
 
