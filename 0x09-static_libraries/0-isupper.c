fc6900e4fb519d85249c42ffcd93bb154be96a91
samm-cyber <samrodoyo@gmail.com>
2022-09-30 21:00:04 -0700

    liby.a

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,1 +1,11 @@
-int _isupper(int c) {}
+#include "main.h"
+
+/**
+ * _isupper - checks if a character is uppercase or not
+ * @c: character is to be tested
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+	{
+		return (c >= 'A' && c <= 'Z');
+	}
