4b73be781d05d9f3d55ab3cfe16da7327e12c912
samm-cyber <samrodoyo@gmail.com>
2022-10-25 17:44:30 -0700

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - deletes the head node
+ * @head: a double pointer
+ *
+ * Return: empty
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *begin;
+	int n;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+	begin = *head;
+	*head = begin->next;
+	n = begin->n;
+	free(start);
+	return (n);
+}
