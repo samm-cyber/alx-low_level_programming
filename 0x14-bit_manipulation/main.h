a151437ab4c1d5c29f1e2823dce9a6c813216983
samm-cyber <samrodoyo@gmail.com>
2022-10-27 17:54:04 -0700

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define BYTE_LENGTH 8
-
+unsigned long int _power(unsigned int base, unsigned int pow);
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
